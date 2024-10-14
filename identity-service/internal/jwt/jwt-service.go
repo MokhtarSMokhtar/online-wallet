bd419e81682797e544591a5c3da7d2acf474317d
MokhtarSMokhtar <mokhtarsayed72@gmail.com>
2024-10-14 10:17:01 +0300

    middelwares

--- identity-service/internal/jwt/jwt-service.go
+++ identity-service/internal/jwt/jwt-service.go
@@ -5,7 +5,9 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"github.com/MokhtarSMokhtar/online-wallet/identity-service/config"
+	"strings"
 	"time"
 )
 
@@ -34,7 +36,7 @@ func GenerateToken(claims Claims) (string, error) {
 	}
 	configu := config.NewConfig()
 	// Set standard claims
-	claims.Exp = time.Now().Add(3).Unix()
+	claims.Exp = time.Now().Add(time.Hour * 40).Unix()
 	claims.Issuer = configu.ISSUER
 	// Marshal header and claims
 	headerJSON, err := json.Marshal(header)
@@ -66,6 +68,55 @@ func GenerateToken(claims Claims) (string, error) {
 	token := unsignedToken + "." + signatureEncoded
 
 	return token, nil
+}
+
+func ValidateToken(tokenString string) (*Claims, error) {
+	configu := config.NewConfig()
+	secretKey := []byte(configu.JWTSecret)
+	parts := strings.Split(tokenString, ".")
+	if len(parts) != 3 {
+		return nil, errors.New("invalid token")
+	}
+	headerEncoded := parts[0]
+	claimsEncoded := parts[1]
+	signatureEncoded := parts[2]
+
+	unsignedToken := headerEncoded + "." + claimsEncoded
+	// Verify the signature
+
+	signature, err := base64.RawURLEncoding.DecodeString(signatureEncoded)
+	if err != nil {
+		return nil, errors.New("invalid signature encoding")
+	}
+
+	expectedSignature, err := sign(unsignedToken, secretKey)
+	if err != nil {
+		return nil, err
+	}
+
+	if !hmac.Equal(signature, expectedSignature) {
+		return nil, errors.New("invalid token signature")
+	}
+	// Decode the claims
+	claimsJSON, err := base64.RawURLEncoding.DecodeString(claimsEncoded)
+	if err != nil {
+		return nil, errors.New("invalid claims encoding")
+	}
+	var claims Claims
+	err = json.Unmarshal(claimsJSON, &claims)
+	if err != nil {
+		return nil, errors.New("invalid claims")
+	}
+
+	if claims.Issuer != configu.ISSUER {
+		return nil, errors.New("invalid token")
+	}
+	// Check expiration
+	if time.Now().Unix() > claims.Exp {
+		return nil, errors.New("token has expired")
+	}
+
+	return &claims, nil
 }
 
 // sign creates an HMAC SHA256 signature
